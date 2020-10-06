d25254b874ed48a2a7d3db617b8f964a2bfdab16
thinhngo-x <duc-thinh.ngo@polytechnique.edu>
2020-10-06 19:12:18 +0200

    Added Projection

--- sanity_check.py
+++ sanity_check.py
@@ -4,17 +4,30 @@ This is for sanity check.
 Usage:
     sanity_check.py prepare_data
     sanity_check.py AdaIN
+    sanity_check.py Projection
 """
 import sys
 
 from docopt import docopt
 
 import torch
+from torch import nn
 import numpy as np
 import matplotlib.pyplot as plt
 
 from main import prepare_data
-from utils.module import AdaIN
+from utils.module import AdaIN, Projection
+
+
+def init_layers(model):
+    """Reinitialize the layer weights for sanity check."""
+    def init_weights(m):
+        if type(m) == nn.Linear:
+            m.weight.data.fill_(0.5)
+            if m.bias is not None:
+                m.bias.data.fill_(0.1)
+    with torch.no_grad():
+        model.apply(init_weights)
 
 
 def sanity_check_for_prepare_data():
@@ -74,6 +87,81 @@ def sanity_check_for_AdaIN():
         print("Passed!")
 
 
+def sanity_check_for_projection():
+    """Test Projection module."""
+    # a is a Tensor of shape (bs=2, c=2, h=2, w=2, d=1)
+    a = torch.Tensor([[[[[1],
+                         [-2]],
+
+                        [[3],
+                         [-4]]],
+
+
+                       [[[5],
+                         [6]],
+
+                        [[7],
+                         [8]]]],
+
+
+
+                      [[[[9],
+                         [10]],
+
+                        [[11],
+                         [12]]],
+
+
+                       [[[-13],
+                         [14]],
+
+                        [[-15],
+                         [16]]]]])
+
+    inp = a.clone()
+    proj = Projection(2, 1, 4)
+    init_layers(proj)
+    res = proj(inp)
+
+    truth = torch.Tensor([[[[3.1000, 2.1000],
+                            [5.1000, 2.1000]],
+
+                           [[3.1000, 2.1000],
+                            [5.1000, 2.1000]],
+
+                           [[3.1000, 2.1000],
+                            [5.1000, 2.1000]],
+
+                           [[3.1000, 2.1000],
+                            [5.1000, 2.1000]]],
+
+
+                          [[[-0.0190, 12.1000],
+                            [-0.0190, 14.1000]],
+
+                           [[-0.0190, 12.1000],
+                            [-0.0190, 14.1000]],
+
+                           [[-0.0190, 12.1000],
+                            [-0.0190, 14.1000]],
+
+                           [[-0.0190, 12.1000],
+                            [-0.0190, 14.1000]]]])
+
+    if not torch.equal(a, inp):
+        print("The input has been changed!")
+    elif res.shape != truth.shape:
+        print("Failed at checking shape of output!")
+        print("Expected shape: ", truth.shape)
+        print("But got: ", res.shape)
+    elif torch.norm(res - truth) > 1e-5:
+        print("Failed at checking value of output!")
+        print("Expected: \n", truth)
+        print("But got: \n", res)
+    else:
+        print("Passed!")
+
+
 def main():
     """Execute sanity check functions."""
     args = docopt(__doc__)
@@ -86,6 +174,8 @@ def main():
         sanity_check_for_prepare_data()
     elif args['AdaIN']:
         sanity_check_for_AdaIN()
+    elif args['Projection']:
+        sanity_check_for_projection()
     else:
         raise RuntimeError('invalid run mode')
 
--- utils/module.py
+++ utils/module.py
@@ -2,7 +2,32 @@
 
 import torch
 from torch import nn
+import torch.nn.functional as F
 import numpy as np
+
+
+def adain_(x, s, eps=1e-8):
+    """Return a normalized tensor controlled by style s.
+
+    @param x (Tensor) Input of shape (batch_size, c, h, w, (d))
+    @param s (Tensor) Style of shape (batch_size, c, 2)
+
+    @returns out (Tensor) Output of shape (batch_size, c, h, w, (d))
+    """
+    dims = len(x.shape)
+    m_x = torch.mean(x, dim=tuple(range(2, dims)), keepdim=True)
+    std_x = torch.std(x, dim=tuple(range(2, dims)), keepdim=True, unbiased=False)  # unbiased or not?
+
+    # print("Mean of x: ", m_x)
+    # print("Std of x: ", std_x)
+
+    while(len(s.shape) < len(x.shape) + 1):
+        s = s.unsqueeze(3)
+        # print(s.shape)
+    # print(s[:, :, 1, :].shape)
+    out = s[:, :, 0] * (x - m_x) / (std_x + eps) + s[:, :, 1]
+
+    return out
 
 
 class AdaIN(nn.Module):
@@ -18,21 +43,33 @@ class AdaIN(nn.Module):
         @param x (Tensor) Input of shape (batch_size, c, h, w, (d))
         @param s (Tensor) Style of shape (batch_size, c, 2)
         """
-        dims = len(x.shape)
-        m_x = torch.mean(x, dim=tuple(range(2, dims)), keepdim=True)
-        std_x = torch.std(x, dim=tuple(range(2, dims)), keepdim=True, unbiased=False)  # unbiased or not?
-
-        # print("Mean of x: ", m_x)
-        # print("Std of x: ", std_x)
-
-        while(len(s.shape) < len(x.shape) + 1):
-            s = s.unsqueeze(3)
-            # print(s.shape)
-        # print(s[:, :, 1, :].shape)
-        x = s[:, :, 0] * (x - m_x) / (std_x + eps) + s[:, :, 1]
-
-        return x
+        return adain_(x, s, eps=eps)
 
 
 class Projection(nn.Module):
     """Projection unit."""
+
+    def __init__(self, c_in: int, d_in: int, c_out: int):
+        """Initialize.
+
+        @param c_in (int) Number of channels of input
+        @param d_in (int) Depth of input
+        @param c_out (int) Number of channels of output
+        """
+        super(Projection, self).__init__()
+        self.linear = nn.Linear(c_in * d_in, c_out)
+
+    def forward(self, x):
+        """Return the projection of 4D-tensor x onto 3D-tensor space.
+
+        @param x (Tensor) Input of shape (batch_size, c_in, h, w, d)
+
+        returns out (Tensor) Output of shape (batch_size, c_out, h, w)
+        """
+        x = torch.transpose(x, 1, 3)  # (batch_size, w, h, c_in, d)
+        bs, w, h, c, d = x.shape
+        x = x.view(bs, w, h, c * d)
+        out = F.leaky_relu(self.linear(x))  # (batch_size, w, h, c_out)
+        out = torch.transpose(out, 1, 3)
+
+        return out
