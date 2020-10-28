5f4ce14724108617d7d4461a79a7a114fed87652
thinhngo-x <duc-thinh.ngo@polytechnique.edu>
2020-10-28 21:18:19 +0100

    device

--- utils/functional.py
+++ utils/functional.py
@@ -29,7 +29,7 @@ def matplotlib_imshow(img, one_channel=False):
     if one_channel:
         img = img.mean(dim=0)
     img = img / 2 + 0.5     # unnormalize
-    npimg = img.to(cpu).numpy()
+    npimg = img.to('cpu').numpy()
     if one_channel:
         plt.imshow(npimg, cmap="Greys")
     else:
