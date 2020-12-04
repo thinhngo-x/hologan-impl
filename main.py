aa983350e5f1b35f5269ba85eb5a13554345d7c4
thinhngo-x <duc-thinh.ngo@polytechnique.edu>
2020-12-04 10:23:18 +0100

    debug

--- main.py
+++ main.py
@@ -170,8 +170,8 @@ def train_one_epoch(dataloader, model: HoloGAN.Net, criterion, optim_G, optim_D,
                 writer.add_scalar("lossD_real/" + name, running_loss_D_real[j] / print_step, step)
                 writer.add_scalar("lossD_fake/" + name, running_loss_D_fake[j] / print_step, step)
             sample_image = functional.gen_sample_images(model, z_dim,
-                                                        [0, 90, 180, -90],
-                                                        [0, 0, 0, 0],
+                                                        [-60, -45, 0, 45, 60],
+                                                        [0, 0, 0, 0, 0],
                                                         device)
             writer.add_image("sample_image", sample_image,
                              global_step=epoch * num_iter + i + 1)
